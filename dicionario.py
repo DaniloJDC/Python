b0e25ea7238cb524ec7646f2332724c8395f82d5
Danilo Chaves <danjunchaves@gmail.com>
2026-09-24 22:13:02 -0300

    Create dicionario.py

---
+++ dicionario.py
@@ -0,0 +1,6 @@
+cinema = []
+filme = {'titulo': 'Star  Wars', 'ano': '1977', 'diretor': 'George Lucas'}
+filme2 = {'tiulo': 'Odisseia', 'ano': '2026', 'diretor': 'Cristopher Nolan'}
+cinema.append(filme)
+cinema.append(filme2)
+
